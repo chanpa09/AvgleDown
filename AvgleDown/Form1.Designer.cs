c6da86fcc62eaa730266d366452a3e7b3b14f51f
chanpa09 <36052882+chanpa09@users.noreply.github.com>
2020-09-21 01:04:48 +0900

    Initial commit

---
+++ AvgleDown/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace AvgleDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.f_path = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.f_downPath = new System.Windows.Forms.TextBox();
+            this.f_newName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // f_path
+            // 
+            this.f_path.Location = new System.Drawing.Point(113, 54);
+            this.f_path.Name = "f_path";
+            this.f_path.Size = new System.Drawing.Size(624, 19);
+            this.f_path.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Path";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "NewName";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "DownPath";
+            // 
+            // f_downPath
+            // 
+            this.f_downPath.Location = new System.Drawing.Point(113, 96);
+            this.f_downPath.Name = "f_downPath";
+            this.f_downPath.Size = new System.Drawing.Size(624, 19);
+            this.f_downPath.TabIndex = 8;
+            this.f_downPath.Text = "C:\\Users\\chanp\\Desktop\\test\\test";
+            // 
+            // f_newName
+            // 
+            this.f_newName.Location = new System.Drawing.Point(113, 139);
+            this.f_newName.Name = "f_newName";
+            this.f_newName.Size = new System.Drawing.Size(624, 19);
+            this.f_newName.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(662, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.f_newName);
+            this.Controls.Add(this.f_downPath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.f_path);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox f_path;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox f_downPath;
+        private System.Windows.Forms.TextBox f_newName;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AvgleDown/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace AvgleDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(f_path.Text.Trim() == "" || f_newName.Text.Trim() == "" || f_downPath.Text.Trim() == "")
+            {
+                MessageBox.Show("Insert path or new name");
+            }
+            else
+            {
+                string l_path = f_path.Text;
+
+                // get path
+                l_path = f_makePath(l_path);
+
+                button1.Enabled = false;
+
+                try
+                {
+                    // down
+                    using (WebClient wc = new WebClient())
+                    {
+                        // set head
+                        wc.Headers.Add("accept: */*");
+                        wc.Headers.Add("accept-encoding: gzip, deflate, br");
+                        wc.Headers.Add("accept-language: ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6");
+                        wc.Headers.Add("origin: https://avgle.com");
+                        wc.Headers.Add("sec-fetch-dest: empty");
+                        wc.Headers.Add("sec-fetch-mode: cors");
+                        wc.Headers.Add("sec-fetch-site: cross-site");
+                        wc.Headers.Add("user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36");
+
+                        for (int i = 1; i <= int.MaxValue; i++)
+                        {
+                            string url = l_path + "/seg-" + i + "-v1-a1.ts";
+                            string down = f_downPath.Text + @"\download" + string.Format("{0:D4}", i) + ".ts";
+                            wc.DownloadFile(url, down);
+                        }
+                    }
+                }
+                catch 
+                {
+                    // make file
+                    string[] inputFilePaths = Directory.GetFiles(f_downPath.Text, "download*.ts");
+                    Console.WriteLine("Number of files: {0}.", inputFilePaths.Length);
+                    using (var outputStream = File.Create(f_downPath.Text + @"\" + f_newName.Text + ".ts"))
+                    {
+                        foreach (var inputFilePath in inputFilePaths)
+                        {
+                            using (var inputStream = File.OpenRead(inputFilePath))
+                            {
+                                // Buffer size can be passed as the second argument.
+                                inputStream.CopyTo(outputStream);
+                            }
+                            File.Delete(inputFilePath);
+                        }
+                    }
+                    MessageBox.Show("Finish!!");
+                }
+                finally
+                {
+                    // 
+                    button1.Enabled = true;
+                }
+            }
+        }
+
+        #region "f_makePath：url생성"
+        /// <summary>
+        /// url생성
+        /// </summary>
+        /// <param name="path">입력한 URL</param>
+        /// <returns></returns>
+        private string f_makePath(string path)
+        {
+            return path.Substring(0, path.LastIndexOf("mp4") + 3);
+        }
+        #endregion
+
+    }
+}
